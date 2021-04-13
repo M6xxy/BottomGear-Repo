d8746af0d27ddafb5a0095ad9adb1346532a7bcb
M6xxy <79139144+M6xxy@users.noreply.github.com>
2021-04-13 16:27:16 +0100

    Movement + Basic Rotation
    Not finished needs some work but its a good base

---
+++ Bottom Gear Project/Assets/Scripts/cameraFollow.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    public Transform playerCamera;
+    public Vector3 offset;
+    void Update()
+    {
+        transform.position = new Vector3(playerCamera.position.x + offset.x, playerCamera.position.y + offset.y, offset.z);
+    }
+}
---
+++ Bottom Gear Project/Assets/Scripts/carSteering.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class carSteering : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()     
+    {
+       
+        //Load rigidbody2d as rb
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+
+        var carVelocity = rb.velocity;
+        print(carVelocity);
+        // Condition: when player presses D add force
+        if (Input.GetKey(KeyCode.D) == true)
+        {
+            //Action: apply a force (move the player)
+            // Get Rigidbody2d to use it
+
+            // Add a force to the rigid body to move player
+            rb.AddForce(Vector2.right * 10f);
+            transform.Rotate(Vector3.back * 1.1f );
+        }
+
+        // Condition: when player presses A add force
+        if (Input.GetKey(KeyCode.A) == true)
+        {
+            //Action: apply a force (move the player)
+            // Get Rigidbody2d to use it
+
+            // Add a force to the rigid body to move player
+            rb.AddForce(Vector2.left * 10f);
+            transform.Rotate(Vector3.forward * 1.1f );
+
+        }
+
+        // Condition: when player presses W add force
+        if (Input.GetKey(KeyCode.W) == true)
+        {
+            //Action: apply a force (move the player)
+            // Get Rigidbody2d to use it
+
+            // Add a force to the rigid body to move player
+            rb.AddForce(Vector2.up * 10f);
+        }
+
+        // Condition: when player presses S add force
+        if (Input.GetKey(KeyCode.S) == true)
+        {
+            //Action: apply a force (move the player)
+            // Get Rigidbody2d to use it
+
+            // Add a force to the rigid body to move player
+            rb.AddForce(Vector2.down * 10f);
+        }
+
+    }
+}
