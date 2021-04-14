9084bb101a23489864e7f98d48fcaf88e47efb2c
M6xxy <79139144+M6xxy@users.noreply.github.com>
2021-04-14 05:24:01 +0100

    Annotated and optimised carSteeringV2

--- Bottom Gear Project/Assets/Scripts/carSteeringv2.cs
+++ Bottom Gear Project/Assets/Scripts/carSteeringv2.cs
@@ -16,34 +16,34 @@ public class carSteeringv2 : MonoBehaviour
         
         Rigidbody2D rb = GetComponent<Rigidbody2D>();
         print(rb.velocity.magnitude);
+        // when player holds W give positive speed * speed multiplier 
         if (Input.GetKey(KeyCode.W) == true)
         {
 
             rb.AddForce(transform.up * speedForce);
             
         }
+        // when player holds S give negative speed
         if (Input.GetKey(KeyCode.S) == true)
         {
-
+            
             rb.AddForce(transform.up * -speedForce / 3.33f);
+            // if player holds S slow down turning speed
             torqueForce = 1f;
 
         }
         else torqueForce = 1.5f;
 
+        // when player holds A add positive torque which turns left
         if (Input.GetKey(KeyCode.A))
         {
             rb.AddTorque(torqueForce * Time.deltaTime, ForceMode2D.Impulse);
         }
 
+        // when player holds D add Negative torque which turns left
         if (Input.GetKey(KeyCode.D))
         {
             rb.AddTorque(-torqueForce * Time.deltaTime, ForceMode2D.Impulse);
         }
-
-        // While player holds S half turnrate
-        
-        else torqueForce = 1.5f;
-        //  rb.AddTorque(Input.GetAxis("Horizontal") * torqueForce * Time.deltaTime, ForceMode2D.Impulse);
     }
 }
